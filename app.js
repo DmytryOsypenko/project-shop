8faff83ed5f99a1b430b970b7b0fee4b8d1d3485
Dmytry <lybovnikitichna@gmail.com>
2025-03-19 21:59:20 +0200

    change like button state

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ let productsCountEl = document.getElementById("products-count");
 
 let addToCartBtns = document.querySelectorAll(".btn2Prise");
 
-console.log(productsCountEl);
-console.log(addToCartBtns);
+// console.log(productsCountEl);
+// console.log(addToCartBtns);
 
 addToCartBtns.forEach((btn) =>
   btn.addEventListener("click", function () {
     productsCountEl.textContent = +productsCountEl.textContent + 1;
+  })
+);
+// change like button state
+let likeBtns = document.querySelectorAll(".IconImg2");
+console.log(likeBtns);
+
+likeBtns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    btn.classList.toggle("liked");
   })
 );
